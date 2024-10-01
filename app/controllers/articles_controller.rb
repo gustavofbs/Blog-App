a1deac9ce154f0f6fd9be78d91178884131d6531
gustavofbs <guguboasorte@gmail.com>
2024-10-01 19:05:22 -0300

    fix rubocop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
-  
   # http_basic_authenticate_with(
   # name: ENV["BASIC_AUTH_USERNAME"],
   # password: ENV["BASIC_AUTH_PASSWORD"],
   # except: [ :index, :show ]
   # )
-  
+
 
   def index
     @articles = Article.all
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-
   # http_basic_authenticate_with(
   # name: ENV["BASIC_AUTH_USERNAME"],
   # password: ENV["BASIC_AUTH_PASSWORD"],
